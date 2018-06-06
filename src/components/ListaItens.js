be2c8b758bb4d915c29f17e55f94ecf9ad11c3e1
Fabio Souza <fabiobarboza7@hotmail.com>
2018-06-06 02:38:55 -0300

    creating components, learning http requests

--- src/components/ListaItens.js
+++ src/components/ListaItens.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
 import {
+  View,
   Text
 } from 'react-native';
+
+import axios from 'axios';
+import Itens from './Itens';
 
 export default class ListaItens extends Component {
 
   constructor(props) {
     super(props);
 
+    this.state = { listaItens: [] };
   }
 
-  componentWillMount(){
-
+  componentWillMount() {
+    // requisição HTTP
+    axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
+      .then( response => { this.setState({ listaItens: response.data }) })
+      .catch(() => { console.log('Error HTML')});
   }
 
-  componentDidMount(){
-
-  }
 
   render() {
     return (
-      <Text>Teste</Text>
+      <View>
+        { this.state.listaItens.map( function(item) { return ( <Text key={item.titulo}>{item.titulo}</Text> ) } ) }
+      </View>
     );
   }
 }
